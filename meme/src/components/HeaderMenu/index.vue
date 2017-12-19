1e5692be8516d508618ed0880f155e8c18d6b19c
199ChenNuo <cn.222@sjtu.edu.cn>
2017-12-19 22:19:45 +0800

    add img upload button
    I want to creat a new page or at least use submenu...

--- meme/src/components/HeaderMenu/index.vue
+++ meme/src/components/HeaderMenu/index.vue
@@ -9,13 +9,20 @@
         <el-menu-item index="1">首页</el-menu-item>
         <span class="header-title">{{ title }}</span>
         <el-menu-item index="2"><el-input v-model="input" placeholder="请输入内容"></el-input></el-menu-item>
-        <el-submenu @command="handleCommand" index="3">
+        <el-submenu index="3">
           <template slot="title">我的工作台</template>
-          <el-menu-item command="imgUpload" index="3-1">上传图片</el-menu-item>
+          <el-menu-item index="3-1">上传图片</el-menu-item>
           <el-menu-item index="3-2">选项2</el-menu-item>
           <el-menu-item index="3-3">选项3</el-menu-item>
         </el-submenu>
-        <el-menu-item index="4">123</el-menu-item>
+        <el-upload
+          index = 4
+          class="imgUpload"
+          :on-preview="hanflePreview"
+          :on-exceed="handleExceed"
+          :file-list="fileList">
+          <el-button size="small" type="primary">点击上传</el-button>
+        </el-upload>
       </el-menu>
 </header>
 </div>
@@ -26,12 +33,11 @@ export default {
   name: 'HeaderMenu',
   props: ['title'],
   methods: {
-    handleSelect: (key, keyPath) => {
-      if (key === '3') {
-        if (keyPath === '3-1') {
-          console.log('img uploading')
-        }
-      }
+    handleSelect: () => {
+      
+    },
+    handleRemove(file, fileList) {
+
     }
   },
 
