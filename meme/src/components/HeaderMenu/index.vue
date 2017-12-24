096bc13e50dc676ce05276af760a30e294342915
sunriseL <540729251@qq.com>
2017-12-24 14:26:35 +0800

    complete user page

--- meme/src/components/HeaderMenu/index.vue
+++ meme/src/components/HeaderMenu/index.vue
@@ -4,12 +4,6 @@
       <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :router="true">
         <el-menu-item  index="display">首页</el-menu-item>
         <el-menu-item :route="''" class="header-search"index=""><el-input v-model="input" placeholder="请输入内容"></el-input></el-menu-item>
-        <el-submenu class="menu" index="3">
-          <template slot="title">我的工作台</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-submenu>
         <el-menu-item  :route="loginRoute" class="user-profile" index="display">{{ isLogin?user:log }}</el-menu-item>
         <span class="header-title">{{ title }}</span>
       </el-menu>
@@ -41,7 +35,7 @@ export default {
   },
   data: () => ({
     activeIndex: '0',
-    input: '搜索',
+    input: '',
     routerAvaliable: true,
     log: '登录',
     user: '个人中心'
--- meme/src/components/UserProfile/index.vue
+++ meme/src/components/UserProfile/index.vue
@@ -1,20 +1,35 @@
 <template>
   <div id="user-page">
-    <el-row>
-    <el-col :span="6">
-        <img class="to" id="avatar" alt="aaa" src="static/5.gif">
+    <el-row class="info">
+    <el-col :span="6" class="hidden-sm-and-down">
+        <img class="to" id="avatar" alt="aaa" src="static/4.jpg">
     </el-col>
     <el-col :span="6">
-      <p id="head">已收藏表情</p>
+      <p id="head">用户名</p>
+      <p id="introduction">用户介绍</p>
     </el-col>
     <el-col :span="12">
       <p id="head">已收藏表情</p>
     </el-col>
     </el-row>
+    <div class="favourite">
+      <p id="head">已收藏表情</p>
+    <block :pictures="pictures"></block>
+    </div>
+    <div class="upload">
+      <div class="top-head">
+      <span id="head">已上传表情</span>
+      <el-button id="upload-button">上传</el-button>
+      </div>
+    <block :pictures="pictures"></block>
+    </div>
+
   </div>
 </template>
 
 <script>
+import 'element-ui/lib/theme-chalk/display.css';
+import Block from 'components/Block'
 export default {
   name: 'UserProfile',
   props: [],
@@ -24,9 +39,30 @@ export default {
     }
   },
   data: () => ({
-
+    pictures: [
+      {
+        url: 'static/1.jpg',
+        text: '狗狗我'
+      }, {
+        url: 'static/2.png',
+        text: 'pingu'
+      }, {
+        url: 'static/3.jpg',
+        text: '熊猫'
+      }, {
+        url: 'static/4.jpg',
+        text: '熊猫'
+      }, {
+        url: 'static/5.gif',
+        text: '萌豚'
+      }, {
+        url: 'static/2.png',
+        text: 'pingu'
+      }
+    ]
   }),
   components: {
+    'block': Block
   }
 }
 </script>
